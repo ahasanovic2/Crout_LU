8c6b4b76bf724f9c39569b7dcddf9c2b49ec722e
Ahmedin Hasanovic <hasanovicahmedin6@gmail.com>
2023-01-01 11:37:54 +0100

    Implemented 2 cholesky methods

--- lu-decomposition.cpp
+++ lu-decomposition.cpp
@@ -2,10 +2,16 @@
 #include <cstdlib>
 #include <random>
 #include <chrono>
+#include <Eigen/Dense>
 #include <omp.h>
+#include <cmath>
+
+using std::endl;
+using std::cout;
+using namespace Eigen;
 
 const int NUM_OF_TRIES = 3;
-const int MATRIX_SIZE = 200;
+const int MATRIX_SIZE = 5000;
 const int NUM_OF_THREADS = 2;
 
 class Timer {
@@ -27,50 +33,6 @@ public:
     }
 };
 
-// Helper functions that are used for clean code
-
-double** generateMatrixA() {
-    double** A = (double**)malloc(sizeof(double*) * MATRIX_SIZE);
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        A[i] = (double*)malloc(sizeof(double) * MATRIX_SIZE);
-    }
-
-    std::random_device rd;
-    std::default_random_engine generator(rd());
-    std::uniform_real_distribution<double> distribution(1, 1000);
-
-
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        for (int j = 0; j < MATRIX_SIZE; j++) {
-            A[i][j] = distribution(generator);
-        }
-    }
-
-    return A;
-}
-
-double** generateMatrixLU() {
-    double** A = (double**)malloc(sizeof(double*) * MATRIX_SIZE);
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        A[i] = (double*)malloc(sizeof(double) * MATRIX_SIZE);
-    }
-
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        for (int j = 0; j < MATRIX_SIZE; j++) {
-            A[i][j] = 0;
-        }
-    }
-
-    return A;
-}
-
-void deallocateMemory(double** A) {
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        free(A[i]);
-    }
-    free(A);
-}
-
 void printMatrix(double** A) {
     std::cout << "Matrix elements are: " << std::endl;
     for (int i = 0; i < MATRIX_SIZE; i++) {
@@ -81,11 +43,51 @@ void printMatrix(double** A) {
     }
 }
 
-/*-----------------------------
-Crout algorithm for LU decomposition
------------------------------*/
-// Sequential version of Crout algorithm
-int crout_0(double** A, double** L, double** U, int n) {
+// ------------------------ Crout LU decomposition ------------------ //
+
+double** generateMatrixA(int n) {
+    double** A = (double**)malloc(sizeof(double*) * n);
+    for (int i = 0; i < n; i++) {
+        A[i] = (double*)malloc(sizeof(double) * n);
+    }
+
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_real_distribution<double> distribution(1, 1000);
+
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i][j] = distribution(generator);
+        }
+    }
+
+    return A;
+}
+
+double** generateMatrixLU(int n) {
+    double** A = (double**)malloc(sizeof(double*) * n);
+    for (int i = 0; i < n; i++) {
+        A[i] = (double*)malloc(sizeof(double) * n);
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            A[i][j] = 0;
+        }
+    }
+
+    return A;
+}
+
+void deallocateMemory(double** A, int n) {
+    for (int i = 0; i < n; i++) {
+        free(A[i]);
+    }
+    free(A);
+}
+
+int crout_sequential(double** A, double** L, double** U, int n) {
     int i, j, k;
     double sum = 0;
     for (i = 0; i < n; i++) {
@@ -113,15 +115,14 @@ int crout_0(double** A, double** L, double** U, int n) {
     return 1;
 }
 
-// Parallel version of Crout algorithm
-int crout_1_runtime(double** A, double** L, double** U, int n) {
+int crout_parallel(double** A, double** L, double** U, int n) {
     int i, j, k;
     double sum = 0;
 #pragma omp parallel for
     for (i = 0; i < n; i++) {
         U[i][i] = 1;
     }
-#pragma omp parallel for private(i,j,k,sum) schedule(runtime)
+#pragma omp parallel for private(i,j,k,sum) schedule(static)
     for (j = 0; j < n; j++) {
         if (j == n - 1)
             std::cout << "Number of threads being used: " << omp_get_num_threads() << std::endl;
@@ -147,161 +148,13 @@ int crout_1_runtime(double** A, double** L, double** U, int n) {
     return 1;
 }
 
-int crout_1_dynamic(double** A, double** L, double** U, int n) {
-    int i, j, k;
-    double sum = 0;
-#pragma omp parallel for
-    for (i = 0; i < n; i++) {
-        U[i][i] = 1;
-    }
-#pragma omp parallel for private(i,j,k,sum) schedule(dynamic)
-    for (j = 0; j < n; j++) {
-        if (j == n - 1)
-            std::cout << "Number of threads being used: " << omp_get_num_threads() << std::endl;
-        for (i = j; i < n; i++) {
-            sum = 0;
-            for (k = 0; k < j; k++) {
-                sum = sum + L[i][k] * U[k][j];
-            }
-            L[i][j] = A[i][j] - sum;
-        }
-        for (i = j; i < n; i++) {
-            sum = 0;
-            for (k = 0; k < j; k++) {
-                sum = sum + L[j][k] * U[k][i];
-            }
-            if (L[j][j] == 0) {
-                std::cout << "Matrix is singular! " << std::endl;
-                exit(0);
-            }
-            U[j][i] = (A[j][i] - sum) / L[j][j];
-        }
-    }
-    return 1;
-}
-
-/*-----------------------------
-Partial pivoting algorithm for LU decomposition
------------------------------*/
-
-static double matrix[MATRIX_SIZE][MATRIX_SIZE];
-static double matrix_p[MATRIX_SIZE][MATRIX_SIZE];
-const double epsilon = 1e-5;
-static int perm[MATRIX_SIZE];
-static int perm_p[MATRIX_SIZE];
-
-// Sequential version of Partial pivoting algorithm
-void partial_pivoting() {
-    int i, j, k, pindex;
-    double* lu = &matrix[0][0];
-    double* row = lu;
-    double* pivot_row = nullptr;
-    double max;
-
-    // for each row and column, k = 0, ..., n-1
-    for (k = 0; k < MATRIX_SIZE; ++k) {
-        pindex = k;
-        max = fabs(*(row + k));
-        {
-            // find the pivot row
-            for (j = k + 1; j < MATRIX_SIZE; ++j) {
-                double temp = fabs(lu[j * MATRIX_SIZE + k]);
-                if (max < temp) {
-                    max = temp;
-                    pindex = j;
-                    pivot_row = &lu[j * MATRIX_SIZE];
-                }
-            }
-            // and if the pivot row differs from the current row
-            // then interchange the two rows
-            if (pindex != k)
-                for (j = 0; j < MATRIX_SIZE; ++j)
-                    std::swap(*(row + j), *(pivot_row + j));
-
-            // and if the matrix is singular, return error
-            if (fabs(*(row + k)) < epsilon)
-                throw std::runtime_error("Matrix is singular");
-
-            // otherwise find the upper triangular matrix elements for row k
-            for (j = k + 1; j < MATRIX_SIZE; ++j)
-                *(row + j) /= *(row + k);
-
-            // update remaining matrix
-            for (i = k + 1; i < MATRIX_SIZE; ++i)
-                for (j = k + 1; j < MATRIX_SIZE; ++j)
-                    lu[i * MATRIX_SIZE + j] -= lu[i * MATRIX_SIZE + k] * *(row + j);
-        }
-        std::swap(perm[pindex], perm[k]);
-        row += MATRIX_SIZE;
-    }
-}
-
-// Parallel version of Partial pivoting algorithm
-void partial_pivoting_parallel() {
-    int i, j, k, pindex;
-    double* lu = &matrix_p[0][0];
-    double* row = lu;
-    double* pivot_row;
-    double max;
-
-    // for each row and column, k = 0, ..., n-1
-    for (k = 0; k < MATRIX_SIZE; ++k) {
-
-        pindex = k;
-        max = fabs(*(row + k));
-#pragma omp parallel num_threads(NUM_OF_THREADS), default(none), private(i, j), shared(k, lu, row, perm_p, pindex, pivot_row, max)
-        {
-            // find the pivot row
-#pragma omp for
-            for (j = k + 1; j < MATRIX_SIZE; ++j) {
-                double temp = fabs(lu[j * MATRIX_SIZE + k]);
-#pragma omp critical
-                if (max < temp) {
-                    max = temp;
-                    pindex = j;
-                    pivot_row = &lu[j * MATRIX_SIZE];
-                }
-            }
-
-            // and if the pivot row differs from the current row
-            // then interchange the two rows
-            if (pindex != k)
-#pragma omp for
-                for (j = 0; j < MATRIX_SIZE; ++j)
-                    std::swap(*(row + j), *(pivot_row + j));
-
-            // and if the matrix is singular, return error
-            if (fabs(*(row + k)) < epsilon)
-                throw std::runtime_error("Matrix is singular");
-
-            // otherwise find the upper triangular matrix elements for row k
-#pragma omp for
-            for (j = k + 1; j < MATRIX_SIZE; ++j)
-                *(row + j) /= *(row + k);
-
-            // update remaining matrix
-#pragma omp for
-            for (i = k + 1; i < MATRIX_SIZE; ++i)
-                for (j = k + 1; j < MATRIX_SIZE; ++j)
-                    lu[i * MATRIX_SIZE + j] -= lu[i * MATRIX_SIZE + k] * *(row + j);
-        }
-
-        std::swap(perm_p[pindex], perm_p[k]);
-        row += MATRIX_SIZE;
-    }
-}
-
-/*-----------------------------
-Void methods used for testing sequential and parallel Crout algorithm
------------------------------*/
-
-void testCroutSequencial(double** A, int n) {
-    double** L = generateMatrixLU();
-    double** U = generateMatrixLU();
+void crout_sequential_test(double** A, int n) {
+    double** L = generateMatrixLU(n);
+    double** U = generateMatrixLU(n);
 
     {
         Timer t("SEQUENTIAL");
-        int rj = crout_0(A, L, U, n);
+        int rj = crout_sequential(A, L, U, n);
         if (rj) {
             std::cout << "Successful!" << std::endl;
         }
@@ -310,90 +163,32 @@ void testCroutSequencial(double** A, int n) {
         }
     }
 
-    deallocateMemory(L);
-    deallocateMemory(U);
+    deallocateMemory(L, n);
+    deallocateMemory(U, n);
 }
 
-void testCroutParallel_runtime(double** A, int n) {
-    double** L = generateMatrixLU();
-    double** U = generateMatrixLU();
+void crout_parallel_test(double** A, int n) {
+    double** L = generateMatrixLU(n);
+    double** U = generateMatrixLU(n);
 
     {
-        Timer t("RUNTIME CROUT 1");
-        crout_1_runtime(A, L, U, n);
+        Timer t("STATIC CROUT 1");
+        crout_parallel(A, L, U, n);
     }
 
-    deallocateMemory(L);
-    deallocateMemory(U);
+    deallocateMemory(L, n);
+    deallocateMemory(U, n);
 }
 
-void testCroutParallel_dynamic(double** A, int n) {
-    double** L = generateMatrixLU();
-    double** U = generateMatrixLU();
+void crout_test(int n) {
+    double** A = generateMatrixA(n);
 
-    {
-        Timer t("DYNAMIC CROUT 1");
-        crout_1_dynamic(A, L, U, n);
-    }
-
-    deallocateMemory(L);
-    deallocateMemory(U);
-}
-
-/*-----------------------------
-Void methods used for testing sequential and parallel Partial pivoting algorithm
------------------------------*/
-
-void fill_matrices() {
-    std::random_device rd;
-    std::default_random_engine generator(rd());
-    std::uniform_real_distribution<double> distribution(1, 1000);
-
-
-    for (int i = 0; i < MATRIX_SIZE; i++) {
-        for (int j = 0; j < MATRIX_SIZE; j++) {
-            matrix[i][j] = matrix_p[i][j] = distribution(generator);
-        }
-    }
-}
-
-void test_partial_pivoting_sequential() {
-    {
-        Timer t("Partial Pivoting Sequential");
-        try {
-            partial_pivoting();
-        }
-        catch (std::exception e) {
-            std::cout << e.what() << std::endl;
-        }
-    }
-}
-
-void test_partial_pivoting_parallel() {
-    {
-        Timer t("Partial Pivoting Parallel");
-        try {
-            partial_pivoting_parallel();
-        }
-        catch (std::exception e) {
-            std::cout << e.what() << std::endl;
-        }
-    }
-}
-
-/*-----------------------------
-Separate methods for testing Crout and Partial pivoting algorithm
------------------------------*/
-
-void test_crout_runtime() {
-    double** A = generateMatrixA();
-
-    testCroutSequencial(A, MATRIX_SIZE);
+    crout_sequential_test(A, n);
 
     std::cout << std::endl;
 
     std::cout << "Maximum number of threads on current device: " << omp_get_max_threads() << std::endl;
-    std::cout << "Matrix size: " << MATRIX_SIZE << "x" << MATRIX_SIZE << std::endl;
+    std::cout << "Matrix size: " << n << "x" << n << std::endl;
 
     omp_set_dynamic(0);
     omp_set_num_threads(NUM_OF_THREADS);
@@ -401,48 +196,7 @@ void test_crout_runtime() {
     for (int i = 0; i < NUM_OF_TRIES; i++) {
 
         std::cout << "TRY " << i + 1 << ":" << std::endl;
-        testCroutParallel_runtime(A, MATRIX_SIZE);
-        std::cout << std::endl;
-    }
-    
-    omp_set_num_threads(NUM_OF_THREADS*2);
-
-    for (int i = 0; i < NUM_OF_TRIES; i++) {
-
-        std::cout << "TRY " << i + 1 << ":" << std::endl;
-        testCroutParallel_runtime(A, MATRIX_SIZE);
-        std::cout << std::endl;
-    }
-    
-    omp_set_num_threads(NUM_OF_THREADS*4);
-
-    for (int i = 0; i < NUM_OF_TRIES; i++) {
-
-        std::cout << "TRY " << i + 1 << ":" << std::endl;
-        testCroutParallel_runtime(A, MATRIX_SIZE);
-        std::cout << std::endl;
-    }
-
-    deallocateMemory(A);
-}
-
-void test_crout_dynamic() {
-    double** A = generateMatrixA();
-
-    testCroutSequencial(A, MATRIX_SIZE);
-
-    std::cout << std::endl;
-
-    std::cout << "Maximum number of threads on current device: " << omp_get_max_threads() << std::endl;
-    std::cout << "Matrix size: " << MATRIX_SIZE << "x" << MATRIX_SIZE << std::endl;
-
-    omp_set_dynamic(0);
-    omp_set_num_threads(NUM_OF_THREADS);
-
-    for (int i = 0; i < NUM_OF_TRIES; i++) {
-
-        std::cout << "TRY " << i + 1 << ":" << std::endl;
-        testCroutParallel_dynamic(A, MATRIX_SIZE);
+        crout_parallel_test(A, n);
         std::cout << std::endl;
     }
 
@@ -451,7 +205,7 @@ void test_crout_dynamic() {
     for (int i = 0; i < NUM_OF_TRIES; i++) {
 
         std::cout << "TRY " << i + 1 << ":" << std::endl;
-        testCroutParallel_dynamic(A, MATRIX_SIZE);
+        crout_parallel_test(A, n);
         std::cout << std::endl;
     }
 
@@ -460,26 +214,201 @@ void test_crout_dynamic() {
     for (int i = 0; i < NUM_OF_TRIES; i++) {
 
         std::cout << "TRY " << i + 1 << ":" << std::endl;
-        testCroutParallel_dynamic(A, MATRIX_SIZE);
+        crout_parallel_test(A, n);
         std::cout << std::endl;
     }
 
-    deallocateMemory(A);
+    deallocateMemory(A, n);
 }
 
-void test_pivot() {
-    fill_matrices();
-    test_partial_pivoting_sequential();
-    test_partial_pivoting_parallel();
+// ------------------------------------------------------------------ //
+
+
+// ---------------------------- CHOLESKY ---------------------------- //
+
+void makeSymmetric(double** A, int n) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i != j)
+                A[i][j] = A[j][i];
 }
+
+bool is_positive_definite(double** A, int n) {
+    Eigen::Map<Eigen::MatrixXd> M(A[0], n, n);
+    SelfAdjointEigenSolver<Matrix3d> eigensolver(M);
+    Vector3d eigenvalues = eigensolver.eigenvalues();
+    for (int i = 0; i < n; i++)
+        if (eigenvalues[i] <= 0)
+            return false;
+    return true;
+}
+
+void cholesky1_sequential(double** A, double** L, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            double s = 0;
+            for (int k = 0; k < j; k++) {
+                s += L[i][k] * L[j][k];
+            }
+            L[i][j] = (i == j) ? sqrt(A[i][i] - s) : (1.0 / L[j][j] * (A[i][j] - s));
+        }
+    }
+}
+
+void cholesky1_parallel(double** A, double** L, int n) {
+#pragma omp parallel for num_threads(4)
+    for (int i = 0; i < n; i++) {
+        if (i == n-1)
+            std::cout << "Number of threads being used: " << omp_get_num_threads() << std::endl;
+        for (int j = 0; j <= i; j++) {
+            double s = 0;
+            for (int k = 0; k < j; k++) {
+                s += L[i][k] * L[j][k];
+            }
+            L[i][j] = (i == j) ? sqrt(A[i][i] - s) : (1.0 / L[j][j] * (A[i][j] - s));
+        }
+    }
+}
+
+void test_cholesky1 (int n) {
+    double** A = generateMatrixA(n);
+    double** L_S = generateMatrixLU(n);
+    double** L_P = generateMatrixLU(n);
+    makeSymmetric(A, n);
+    {
+        Timer t("Check if matrix is positive definite");
+        cout << "Matrix is " << (is_positive_definite(A, n) ? "not" : "") << "positive definite" << endl;
+    }
+    {
+        Timer t("CHOLESKY 1 SEQUENTIAL");
+        cholesky1_sequential(A, L_S, n);
+    }
+    {
+        Timer t("CHOLESKY 1 PARALLEL");
+        cholesky1_parallel(A, L_P, n);
+    }
+    free(A);
+    free(L_S);
+    free(L_P);
+}
+
+double* generate_matrix(int n) {
+    double* A = (double*)calloc(n * n, sizeof(double));
+    std::random_device rd;
+    std::default_random_engine generator(rd());
+    std::uniform_real_distribution<double> distribution(1, 1000);
+
+
+    for (int i = 0; i < n * n; i++) {
+        A[i] = distribution(generator);
+    }
+
+    return A;
+}
+
+void makeSymmetric2(double* A, int n) {
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            if (i != j)
+                A[j * n + i] = A[i * n + j];
+}
+
+bool is_positive_definite2(double* A, int n) {
+    Matrix3d M = Map<Matrix3d>(A, n, n);
+    SelfAdjointEigenSolver<Matrix3d> eigensolver(M);
+    Vector3d eigenvalues = eigensolver.eigenvalues();
+    for (int i = 0; i < n; i++)
+        if (eigenvalues[i] <= 0)
+            return false;
+    return true;
+}
+
+double* cholesky2_parallel(double* A, int n) {
+    double* L = (double*)calloc(n * n, sizeof(double));
+    if (L == NULL)
+        exit(EXIT_FAILURE);
+
+    for (int j = 0; j < n; j++) {
+        double s = 0;
+        for (int k = 0; k < j; k++) {
+            s += L[j * n + k] * L[j * n + k];
+        }
+        L[j * n + j] = sqrt(A[j * n + j] - s);
+#pragma omp parallel for num_threads(4)
+        for (int i = j + 1; i < n; i++) {
+            double s = 0;
+            for (int k = 0; k < j; k++) {
+                s += L[i * n + k] * L[j * n + k];
+            }
+            L[i * n + j] = (1.0 / L[j * n + j] * (A[i * n + j] - s));
+        }
+    }
+    return L;
+}
+
+double* cholesky2_sequential(double* A, int n) {
+    double* L = (double*)calloc(n * n, sizeof(double));
+    if (L == NULL)
+        exit(EXIT_FAILURE);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (i + 1); j++) {
+            double s = 0;
+            for (int k = 0; k < j; k++) {
+                s += L[i * n + k] * L[j * n + k];
+            }
+            L[i * n + j] = (i == j) ? sqrt(A[i * n + i] - s) : (1.0 / L[j * n + j] * (A[i * n + j] - s));
+        }
+    }
+    return L;
+}
+
+void test_cholesky2(int n) {
+    double* A = generate_matrix(n);
+    makeSymmetric2(A, n);
+    {
+        Timer t("Check if matrix is positive definite");
+        cout << "Matrix is " << (is_positive_definite2(A, n) ? "not" : "") << "positive definite" << endl;
+    }
+    {
+        Timer t("CHOLESKY 2 SEQUENTIAL");
+        auto l = cholesky2_sequential(A, n);
+        free(l);
+    }
+    {
+        Timer t("CHOLESKY 2 PARALLEL");
+        auto l = cholesky2_parallel(A, n);
+        free(l);
+    }
+    free(A);
+}
+// ------------------------------------------------------------------ //
 
 int main()
 {
-    //test_pivot();
-    std::cout << "RUNTIME SCHEDULING: " << std::endl;
-    test_crout_runtime();
-    std::cout << "DYNAMIC SCHEDULING: " << std::endl;
-    test_crout_dynamic();
+    /*int n;
+    do {
+        std::cout << "Insert a number that will be dimension for square matrix: ";
+        std::cin >> n;
+        if (n != 0) {
+            std::cout << "STATIC SCHEDULING: " << std::endl;
+            test_crout_dynamic(n);
+        }
+    } while (n != 0);*/
+
+    /*Eigen::Matrix2d A;
+    A << 2, 1,
+        1, 3;
+    if (A == A.transpose())
+        cout << "Jeste simetricna" << endl;
+    else
+        cout << "Nije simetricna" << endl;*/
+    cout << "Insert number of rows for matrix: ";
+    int broj;
+    std::cin >> broj;
+    test_cholesky1(broj);
+    //test_cholesky2(broj);
+    //crout_test(broj);
     return 0;
 
 }
